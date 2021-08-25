f07942cdc610c2f328410d4e70dc8aa550ea7cc5
IIGodBananaII <samuelotigre@gmail.com>
2021-08-24 22:09:02 -0300

    alteramos o banco

--- includes/Constants.php
+++ includes/Constants.php
@@ -2,6 +2,6 @@
 
 
 	define('DB_HOST', 'localhost');
-	define('DB_USER', 'etecia');
-	define('DB_PASS', '123456');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
 	define('DB_NAME', 'android');
